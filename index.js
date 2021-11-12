ab0778809cc83366682d366aaf2eba190fb16e0b
toukubo <toukubo@gmail.com>
2021-11-12 21:00:03 +0900

    1 file modified

--- index.js
+++ index.js
@@ -4,5 +4,4 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
-
-console.log("hogehoge")
+connsole.log('hogehoge');
